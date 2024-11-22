4d4fdc9042bb21a3297894b4130a40dc24eb4212
Eugene Katrukha <katpyxa@gmail.com>
2024-11-22 10:58:43 +0100

    example deskew

---
+++ src/test/java/bvvpg/debug/DebugLLSInterpolation.java
@@ -0,0 +1,75 @@
+package bvvpg.debug;
+
+import java.util.List;
+
+import bdv.spimdata.SpimDataMinimal;
+import bdv.spimdata.XmlIoSpimDataMinimal;
+import bdv.tools.transformation.TransformedSource;
+import bdv.viewer.SourceAndConverter;
+import bvvpg.core.VolumeViewerPanel;
+import bvvpg.vistools.BvvFunctions;
+import bvvpg.vistools.BvvSource;
+import bvvpg.vistools.BvvStackSource;
+import mpicbg.spim.data.SpimDataException;
+
+import net.imglib2.realtransform.AffineTransform3D;
+
+public class DebugLLSInterpolation {
+	public static void main( final String[] args )
+	{
+		
+		final String xmlFilename = "/home/eugene/Desktop/projects/BigTrace/LLS_interp/LLS_interpolation.xml";
+		SpimDataMinimal spimData = null;
+		try {
+			spimData = new XmlIoSpimDataMinimal().load( xmlFilename );
+		} catch (SpimDataException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return;
+		}				
+		
+		//make a deskew transform
+		AffineTransform3D deskew = makeLLS7Transform();
+		
+		final List< BvvStackSource< ? > > sources = BvvFunctions.show( spimData );
+		final BvvSource source = sources.get(0);
+
+		VolumeViewerPanel panel = source.getBvvHandle().getViewerPanel();
+
+		for ( SourceAndConverter< ? > sourceC : panel.state().getSources() )
+		{
+			(( TransformedSource< ? > ) sourceC.getSpimSource() ).setFixedTransform(deskew);
+		}	
+		
+		source.setDisplayRange( 0, 400 );
+
+		AffineTransform3D viewSideAT = source.getBvvHandle().getViewerPanel().state().getViewerTransform();
+		viewSideAT.rotate(1, Math.PI/2 );
+		viewSideAT.rotate(2, (-1.0)*Math.PI/2 );
+		viewSideAT.translate( new double [] {-150.0,-100.0,0.0} );
+		//rotate view so we see the stripes
+		source.getBvvHandle().getViewerPanel().state().setViewerTransform( viewSideAT );
+		
+	}
+	
+	/** function generates new LLS7 transform (rotation/shear/z-scaling **/
+	static AffineTransform3D makeLLS7Transform()
+	{
+		AffineTransform3D afDataTransform = new AffineTransform3D();
+		AffineTransform3D tShear = new AffineTransform3D();
+		AffineTransform3D tRotate = new AffineTransform3D();
+			
+		//rotate 30 degrees
+		tRotate.rotate(0, (-1.0)*Math.PI/6.0);
+		//shearing transform
+		tShear.set(1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.7320508075688767, 0.0, 0.0, 0.0, 1.0, 0.0);
+		//Z-step adjustment transform
+		afDataTransform.set(1.0, 0.0, 0.0, 0.0, 
+								0.0,1.0, 0.0, 0.0, 
+								0.0, 0.0, 0.5, 0.0);
+		
+		afDataTransform = tShear.concatenate(afDataTransform);
+		afDataTransform = tRotate.concatenate(afDataTransform);
+		return afDataTransform;
+	}
+}
